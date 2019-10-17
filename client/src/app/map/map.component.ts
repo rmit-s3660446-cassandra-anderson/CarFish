bc39277602501773eae520296e21481be88e315a
Cass <s3660446@student.rmit.edu.au>
2019-10-17 16:11:10 +1100

    added info windows to the markers

--- client/src/app/map/map.component.ts
+++ client/src/app/map/map.component.ts
@@ -1,3 +1,4 @@
+/// <reference types="googlemaps" />
 import { Component, OnInit, Input, OnChanges, SimpleChanges  } from '@angular/core';
 
 @Component({
@@ -9,92 +10,24 @@ export class MapComponent implements OnInit, OnChanges {
 
   @Input() displayResults: any;
 
-  // we must hard code the starting coordinates
-  // the starting point atm is London
-  latitude = 51.508742;
-  longitude = -0.120850;
+  // we must hard code the starting coordinate and a specified zoom
+  mapLatitude = 0;
+  mapLongitude = 0;
+  zoom = 6;
 
   selectedMarker;
   service: any;
 
   // stores the coordinates and the car info
   // associated with each point
-  markers = [
-    // { lat: 51.508742,
-    //   lng: -0.420000,
-    //   item: {
-    //     carName: "Toyota Corolla",
-    //     style: "hatchback",
-    //     costph: 24
-    //   }
-    // },
-    // { lat: -0.120850,
-    //   lng: 51.508742,
-    //   item: {
-    //     carName: "Honda Civic",
-    //     style: "sedan",
-    //     costph: 15
-    //   }
-    // },
-    // { lat: 42.120850,
-    //   lng: 50.508742,
-    //   item: {
-    //     carName: "Hyundai Getz",
-    //     style: "sedan",
-    //     costph: 20
-    //   }
-    // },
-    // { lat: -51.414131,
-    //   lng: -0.4214123,
-    //   item: {
-    //     carName: "Toyota Camry",
-    //     style: "hatchback",
-    //     costph: 28
-    //   }
-    // },
-    // { lat: 60.414131,
-    //   lng: 60.4214123,
-    //   item: {
-    //     carName: "Range Rover",
-    //     style: "sedan",
-    //     costph: 28
-    //   }
-    // },
-    // { lat: -60.414131,
-    //   lng: -60.4214123,
-    //   item: {
-    //     carName: "Holden Mustang",
-    //     style: "sedan",
-    //     costph: 50
-    //   }
-    // }
-  ];
+  markers = [];
 
-  // since the coordinates are assumed to be unique
-  // they can be used to find the associated car
-  findItem(lat, lng){
-    var item;
-    var i;
-    console.log(lat + " " + lng);
-    for (i = 0; i < this.markers.length; i++){
-      if (lat === this.markers[i].lat && lng === this.markers[i].lng){
-        console.log(this.markers[i].item);
-        item = this.markers[i].item;
-      }
-    }
-    return item;
+  addMarker(location: any, info: any) {
+    this.markers.push({lat: location.lat(), lng: location.lng(), info: info});
   }
 
-  selectMarker(event){
-    this.selectedMarker = {
-      lat: event.latitude,
-      lng: event.longitude,
-      info: this.findItem(event.latitude, event.longitude)
-    };
-  }
-
-  addMarker(location: any) {
-    this.markers.push({lat: location.lat(), lng: location.lng()});
+  resetMarkers() {
+    this.markers = [];
   }
 
   constructor() { }
@@ -106,24 +39,35 @@ export class MapComponent implements OnInit, OnChanges {
   /* Whenever the user searches, the new search results gets passed
      through to here */
   ngOnChanges(changes: SimpleChanges) {
-    console.log("changes");
-    console.log(changes.displayResults.currentValue);
+    console.log("changes", changes.displayResults.currentValue);
+    // clears the current markers off the map
+    this.markers = [];
     changes.displayResults.currentValue.forEach((result) => {
       var request = {
-        query: result.location.street + "," + result.location.suburb,
+        query: result.location.street + ", " + result.location.suburb,
         fields: ['name', 'geometry'],
       };
+
+      var info = {
+        location: result.location.street + ", " + result.location.suburb,
+        car: result.type.year + " " + result.type.brand + " " + result.type.model,
+      };
+      console.log("the result", result);
 
       // this is calling the places api and finding the location based on
       // the street and suburb
       this.service.findPlaceFromQuery(request, (results, status) => {
         if (status === google.maps.places.PlacesServiceStatus.OK) {
           for (var i = 0; i < results.length; i++) {
-            this.addMarker(results[i].geometry.location);
+            this.addMarker(results[i].geometry.location, info);
           }
-          // map.setCenter(results[0].geometry.location);
+          // sets where the map is looking
+          this.mapLatitude = results[0].geometry.location.lat();
+          this.mapLongitude = results[0].geometry.location.lng();
+          this.zoom = 14;
         }
       });
     });
   }
+
 }
